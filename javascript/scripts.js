cb9700a49edff1cb8272493a2ef54f030ad751d4
Genevieve Latimer <genevievelatimer@gmail.com>
2016-03-10 13:11:35 -0800

    Attempt to use conditional statement for js logic.

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,4 +1,18 @@
-//Numbers divisible by 3 are replaced with "ping"
-//Numbers divisible by 5 are replaces with "pong"
-//Numbers divisible by 15 are replaced with "pingpong"
+var number = 
+var PingPong = function(number) {
+  //Numbers divisible by 3 are replaced with "ping"
+  if (number % 3 === 0) {
+    return "ping"
+  //Numbers divisible by 5 are replaces with "pong"
+  } else if (number % 5 === 0) {
+    return "pong"
+  //Numbers divisible by 15 are replaced with "pingpong"
+  } else if (number % 15 === 0) {
+    return "pingpong"
+  } else {
+    return number
+  }
+}
+
+
 //User should be able to enter new numbers and get new results every time
