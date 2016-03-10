faa19f2634677324b8596a99be5faa242f27acf7
Genevieve Latimer <genevievelatimer@gmail.com>
2016-03-10 12:59:51 -0800

    Initialize js folder and set up scripts.js

---
+++ javascript/scripts.js
@@ -0,0 +1,4 @@
+//Numbers divisible by 3 are replaced with "ping"
+//Numbers divisible by 5 are replaces with "pong"
+//Numbers divisible by 15 are replaced with "pingpong"
+//User should be able to enter new numbers and get new results every time
