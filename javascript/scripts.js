4760f5ea7937e0bc6b98b5cbf98e706ac376dfe4
Genevieve Latimer <genevievelatimer@gmail.com>
2016-03-10 14:01:10 -0800

    Begin jquery setup now that javascript is functioning.

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,18 +1,16 @@
-var number = 
-var PingPong = function(number) {
-  //Numbers divisible by 3 are replaced with "ping"
-  if (number % 3 === 0) {
-    return "ping"
-  //Numbers divisible by 5 are replaces with "pong"
-  } else if (number % 5 === 0) {
-    return "pong"
-  //Numbers divisible by 15 are replaced with "pingpong"
-  } else if (number % 15 === 0) {
-    return "pingpong"
-  } else {
-    return number
+var number;
+var pingPong = function(number) {
+    if ((number % 3 === 0) && (number % 15 !== 0)) {
+      return "ping"
+    } else if ((number % 5 === 0) && (number % 15 !== 0)) {
+      return "pong"
+    } else if (number % 15 === 0) {
+      return "pingpong"
+    } else {
+      return number
   }
 }
-
-
+$(document).ready(function() {
+  
+})
 //User should be able to enter new numbers and get new results every time
