2ccfd5513c2ab9efdcee5b1f5d14a7675e5e351e
Genevieve Latimer <genevievelatimer@gmail.com>
2016-03-11 11:25:45 -0800

    WIP Attempt to print a range of numbers using js.

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,20 +1,32 @@
+var pingPong = function(number) {
+  for (var i = 1; i <= number; i += 1) {
+  };
+  return i
+};
+
+
+
 
 var pingPong = function(number) {
+  var count = [];
+  for (var i = 1; i <= number; count += 1) {
+    return
     if ((number % 3 === 0) && (number % 15 !== 0)) {
-      return "ping"
+      count.push("ping");
     } else if ((number % 5 === 0) && (number % 15 !== 0)) {
-      return "pong"
+      count.push("pong");
     } else if (number % 15 === 0) {
-      return "pingpong"
+      count.push("pingpong");
     } else {
       return number
+    }
   }
 }
 $(document).ready(function() {
-  $("form").submit(function(event) {
-    var number = ("form#number").val();
+  $("form#numbers").submit(function(event) {
+    var number = parsint($("input#number").val());
 
-
+    $("#results").write();
     event.preventdefault();
   });
 });
