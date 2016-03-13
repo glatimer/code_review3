cbd1cef58a52096d3b0738a7f87c3ef7249950d8
Genevieve Latimer <genevievelatimer@gmail.com>
2016-03-12 19:32:55 -0800

    Attempt to fix communication between jQuery and HTML.

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,25 +1,25 @@
-  var pingPong = function(integer) {
-    var range = [];
-    for (var i = 1; i <= integer; i += 1) {
-      if ((i % 3 === 0) && (i % 15 !== 0)) {
-        range.push("ping");
-      } else if ((i % 5 === 0) && (i % 15 !== 0)) {
-        range.push("pong");
-      } else if (i % 15 === 0) {
-        range.push("pingpong");
-      } else {
-        range.push(i);
-      };
+var pingPong = function(number) {
+  var range = [];
+  for (var i = 1; i <= number; i += 1) {
+    if ((i % 3 === 0) && (i % 15 !== 0)) {
+      range.push("ping");
+    } else if ((i % 5 === 0) && (i % 15 !== 0)) {
+      range.push("pong");
+    } else if (i % 15 === 0) {
+      range.push("pingpong");
+    } else {
+      range.push(i);
     };
-    return range;
   };
+  var string = range.toString();
+  return string;
+};
+$(document).ready(function() {
+  $("form").submit(function(event) {
+    var number = parseInt($("input#number").val());
 
-  $(document).ready(function() {
-    $("form").submit(function(event) {
-      var integer = parsint($("input#number").val());
-
-      $("#result").text(pingPong);
-      event.preventdefault();
-    });
+  $("#result").text(pingPong);
+  event.preventDefault();
+  });
 });
 //User should be able to enter new numbers and get new results every time
